aad4a4258645beea8c08ef5b69f1771eb59e2737
OstafiichukO <85893003+OstafiichukO@users.noreply.github.com>
2022-04-25 13:45:49 +0300

    store cleaning

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import { itemsReducer, filterSlice } from "./itemsSlice";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+
+export const addContact = createAction("contacts/addContact");
+export const removeContact = createAction("contacts/removeContact");
+export const handlerFilter = createAction("contacts/handleFilter");
+
+const reducerContacts = createReducer(
+  {
+    items: [],
+    filter: "",
+  },
+  {
+    [addContact]: (state, action) => void state.items.push(action.payload),
+    [removeContact]: (state, action) => {
+      const contactsList = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+      state.items = contactsList;
+    },
+    [handlerFilter]: (state, action) => void (state.filter = action.payload),
+  }
+);
 
 export const store = configureStore({
   reducer: {
-    items: itemsReducer,
-    filter: filterSlice.reducer,
-  },
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
+    contacts: reducerContacts,
   },
 });
-
-export const persistor = persistStore(store);
