7de56a2537b7a133b58c3f4d046f016f27070bdd
OstafiichukO <85893003+OstafiichukO@users.noreply.github.com>
2022-04-25 15:51:27 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,20 +1,45 @@
-import PropTypes from "prop-types";
-import Contact from "./Contact";
-import { StyledUl } from "./ContactList.styled";
+import {
+  StyledUl,
+  StyledLi,
+  StyledSpan,
+  StyledButton,
+} from "./ContactList.styled";
 
-const ContactsList = ({ contacts, onClick }) => {
+import { useSelector, useDispatch } from "react-redux";
+import { removeContact } from "../../redux/store";
+
+const ContactsList = () => {
+  const dispatch = useDispatch();
+  const contactsShown = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.contacts.filter);
+
+  let contacts = "";
+  if (contactsShown) {
+    contacts = contactsShown.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
   return (
     <>
       <StyledUl>
-        <Contact contacts={contacts} onClick={onClick} />
+        {contacts &&
+          contacts.map(({ id, name, phone }) => (
+            <StyledLi key={id}>
+              <StyledSpan>
+                &#9742; {name}: {phone}
+              </StyledSpan>
+              <StyledButton
+                type="button"
+                name={id}
+                onClick={() => dispatch(removeContact(id))}
+              >
+                Delete
+              </StyledButton>
+            </StyledLi>
+          ))}
       </StyledUl>
     </>
   );
 };
 
 export default ContactsList;
-
-ContactsList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
